ba83cd7d943151036dbd1ce1dcfcafc8a1313f8c
I-SumitDabral <sumitdabral987@gmail.com>
2026-04-19 21:46:40 +0530

    code changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,12 +4,12 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon-16x16.png": "5b7c1b506dc96673623bb04d47dccf55",
-"flutter_bootstrap.js": "a4c635f7402ad7f3782de9dc4c2b333c",
+"flutter_bootstrap.js": "e4575361f98dcb9242a7a617a99e6dcf",
 "version.json": "009c9e65172e010890f7f65fde438006",
 "favicon.ico": "c3da7656fc6e6379f08f682c7232d790",
 "index.html": "844a4c0625c4d328f4c00d80eb5f0974",
 "/": "844a4c0625c4d328f4c00d80eb5f0974",
-"main.dart.js": "9c7dba06b764e6aa8cf0eab4dceabceb",
+"main.dart.js": "fa3ec95d0e133e4a539e988c5f54383b",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "icons/android-chrome-192x192.png": "126757f52aee4dd819adb4c45034b966",
 "icons/apple-touch-icon.png": "31e789423a0e6999dfd41b3bf729654b",
